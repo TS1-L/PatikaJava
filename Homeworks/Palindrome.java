d45d19c0e865a6648fa081ca34f2fadaae8909d4
NEDAL ZAWAIDI <2010206512@ogrenci.karabuk.edu.tr>
2025-03-04 03:54:16 +0300

    Create Palindrome.java

---
+++ Homeworks/Palindrome.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome number.");
+        } else {
+            System.out.println(number + " is not a palindrome number.");
+        }
+
+        input.close();
+    }
+
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number = number / 10;
+        }
+
+        return originalNumber == reversedNumber;
+
+
+    }
+}
