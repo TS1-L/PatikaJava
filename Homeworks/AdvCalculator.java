6c48d11b8ff7f14e81ad060609654558992040a1
NEDAL ZAWAIDI <2010206512@ogrenci.karabuk.edu.tr>
2025-03-06 01:15:24 +0300

    Create AdvCalculator.java

---
+++ Homeworks/AdvCalculator.java
@@ -0,0 +1,188 @@
+import java.util.Scanner;
+
+public class AdvCalculator {
+
+    static void plus() {
+        Scanner input = new Scanner(System.in);
+        int number, result = 0, i = 1;
+        while (true) {
+            System.out.print(i++ + ". number :");
+            number = input.nextInt();
+            if (number == 0) {
+                break;
+            }
+            result += number;
+        }
+        System.out.println("Result : " + result);
+    }
+
+    static void minus() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many numbers will you enter :");
+        int counter = scan.nextInt();
+        int number, result = 0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". number :");
+            number = scan.nextInt();
+            if (i == 1) {
+                result += number;
+                continue;
+            }
+            result -= number;
+        }
+
+        System.out.println("Result : " + result);
+    }
+
+    static void times() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 1, i = 1;
+
+        while (true) {
+            System.out.print(i++ + ". number :");
+            number = scan.nextInt();
+
+            if (number == 1)
+                break;
+
+            if (number == 0) {
+                result = 0;
+                break;
+            }
+            result *= number;
+        }
+
+        System.out.println("Result : " + result);
+    }
+
+    static void divided() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many numbers will you enter :");
+        int counter = scan.nextInt();
+        double number, result = 0.0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". number :");
+            number = scan.nextDouble();
+            if (i != 1 && number == 0) {
+                System.out.println("You cannot enter 0 as a divisor.");
+                continue;
+            }
+            if (i == 1) {
+                result = number;
+                continue;
+            }
+            result /= number;
+        }
+
+        System.out.println("Result : " + result);
+    }
+
+    static void power() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the base value :");
+        int base = scan.nextInt();
+        System.out.print("Enter the exponent value :");
+        int exponent = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println("Result : " + result);
+    }
+
+    static void factorial() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number :");
+        int n = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        System.out.println("Result : " + result);
+    }
+
+    // New method for modulus operation
+    static void mod() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number to take the modulus of :");
+        int num = scan.nextInt();
+        System.out.print("Enter the modulus value :");
+        int mod = scan.nextInt();
+
+        int result = num % mod;
+        System.out.println("Result : " + result);
+    }
+
+    // New method for rectangle area and perimeter calculation
+    static void rectangle() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle :");
+        double length = scan.nextDouble();
+        System.out.print("Enter the width of the rectangle :");
+        double width = scan.nextDouble();
+
+        double area = length * width;
+        double perimeter = 2 * (length + width);
+
+        System.out.println("Area of the rectangle : " + area);
+        System.out.println("Perimeter of the rectangle : " + perimeter);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int select;
+        String menu = "1- Addition\n"
+                + "2- Subtraction\n"
+                + "3- Multiplication\n"
+                + "4- Division\n"
+                + "5- Exponentiation\n"
+                + "6- Factorial\n"
+                + "7- Modulus\n"
+                + "8- Rectangle Area and Perimeter\n"
+                + "0- Exit";
+
+        do {
+            System.out.println(menu);
+            System.out.print("Please select an operation :");
+            select = scan.nextInt();
+            switch (select) {
+                case 1:
+                    plus();
+                    break;
+                case 2:
+                    minus();
+                    break;
+                case 3:
+                    times();
+                    break;
+                case 4:
+                    divided();
+                    break;
+                case 5:
+                    power();
+                    break;
+                case 6:
+                    factorial();
+                    break;
+                case 7:
+                    mod();
+                    break;
+                case 8:
+                    rectangle();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("You entered an invalid value, please try again.");
+            }
+        } while (select != 0);
+
+        scan.close();
+    }
+}
