2907af9d21f20adcf4d3d1395e4683df8aef9d5f
NEDAL ZAWAIDI <2010206512@ogrenci.karabuk.edu.tr>
2025-03-15 02:52:51 +0300

    Create HarmonicAvg.java

---
+++ Homeworks/HarmonicAvg.java
@@ -0,0 +1,22 @@
+
+public class HarmonicAvg {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5}; 
+        double harmonicSum = 0; 
+
+        
+        for (int i = 0; i < numbers.length; i++) {
+            harmonicSum += 1.0 / numbers[i]; 
+        }
+
+        
+        double harmonicMean = numbers.length / harmonicSum;
+
+        
+        System.out.println("Harmonic Average is: " + harmonicMean);
+
+
+    }
+}
